fb402d649cb14931764568a6b021ced04b365f29
Val <valeriegzz0204@gmail.com>
2026-02-04 22:13:07 -0600

    Add files via upload

---
+++ LA4_Group2/HEALTHCLUBFEE/Form1.vb
@@ -0,0 +1,97 @@
+﻿'Group 2 (Valerie Gonzalez, Joseph Kang, Joshua Pacheco, Samantha Vera)
+'July 21, 2025
+'This is our Lab Assignment 4 - (Health Club Fee)
+
+Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim decBaseFee As Decimal           ' Base Monthly Fee
+        Dim decTotalFee As Decimal          ' Total Membership Fee
+        Dim intMonths As Integer            ' Number of Months
+        Dim blnInputOk As Boolean = True    ' flag indicator if number of months entered is valid
+
+        ' Constants for base fees.
+        Const decADULT_FEE As Decimal = 40D
+        Const decCHILD_FEE As Decimal = 20D
+        Const decSTUDENT_FEE As Decimal = 25D
+        Const decSENIOR_FEE As Decimal = 30D
+
+        'Constants for addtional fees.
+        Const decYOGA_FEE As Decimal = 10D
+        Const decKARATE_FEE As Decimal = 30D
+        Const decTRAINER_FEE As Decimal = 50D
+
+        ' Validate and convert the number of months.
+        lblStatus.Text = String.Empty
+        If Integer.TryParse(txtMonths.Text, intMonths) = False Then
+            lblStatus.Text = "Months must be an integer."
+            blnInputOk = False
+
+
+            ' Validate the number of months.
+        ElseIf intMonths < 1 Or intMonths > 24 Then
+            lblStatus.Text = "Months must be in the range 1 - 24."
+            blnInputOk = False
+        End If
+
+        If blnInputOk = True Then
+            ' Determine the base monthy fee.
+            If radAdult.Checked = True Then
+                decBaseFee = decADULT_FEE
+            ElseIf radChild.Checked = True Then
+                decBaseFee = decCHILD_FEE
+            ElseIf radStudent.Checked = True Then
+                decBaseFee = decSTUDENT_FEE
+            ElseIf radSenior.Checked = True Then
+                decBaseFee = decSENIOR_FEE
+            End If
+
+            ' Check for additional services.
+            If chkYoga.Checked = True Then
+                decBaseFee += decYOGA_FEE
+            End If
+
+            If chkKarate.Checked = True Then
+                decBaseFee += decKARATE_FEE
+            End If
+
+            If chkTrainer.Checked = True Then
+                decBaseFee += decTRAINER_FEE
+            End If
+
+            ' Calculate the total fee.
+            decTotalFee = decBaseFee * intMonths
+
+            ' Display the fees.
+            lblMonthlyFee.Text = decBaseFee.ToString("c")
+            lblTotalFee.Text = decTotalFee.ToString("c")
+
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Reset the Adult radio button.
+        radAdult.Checked = True
+
+        ' Clear the check boxes.
+        chkYoga.Checked = False
+        chkKarate.Checked = False
+        chkTrainer.Checked = False
+
+        'Clear the number of months.
+        txtMonths.Clear()
+
+        'Clear the fee labels.
+        lblMonthlyFee.Text = String.Empty
+        lblTotalFee.Text = String.Empty
+        lblStatus.Text = String.Empty
+
+        'Give txtMonths the focus.
+        txtMonths.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form.
+        Me.Close()
+    End Sub
+End Class
